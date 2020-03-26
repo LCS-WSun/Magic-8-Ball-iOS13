67533aa7a55bc8820e53e062bd31ba33bb048b6e
Wesley Sun <wsun@lcs.on.ca>
2020-03-26 17:01:57 -0400

    Finished the app with implementing randomization, connecting outlets and actions and using a array

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -9,10 +9,39 @@
 import UIKit
 
 class ViewController: UIViewController {
+   
+        //The image view as a outlet
+    @IBOutlet weak var imageView: UIImageView!
     
+    
+    
+//        The button as a action
+    @IBAction func askButton(_ sender: UIButton) {
+    
+    //A constant array that does not need to be mutated with 
     let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
+                
+    
+//    imageView.image = ballArray[Int.random(in: 0...4)] or you can express it as randomElement()
+        
+        
+    //Randomizing by using randomElement() in the array to pick a value from 0-4
+     imageView.image = ballArray.randomElement()
 
+        
 
-
+    //String concatenation
+    print("Image is\(ballArray)")
+        
+    }
+        
+        
+        
+        
+    
 }
+    
+    
+    
+    
 
